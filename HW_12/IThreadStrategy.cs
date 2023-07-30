80097889b1c86c06ebb26a8e3d8fc718e02ab4e3
philimonovoleg <philimonovoleg@gmail.com>
2023-07-30 18:03:36 +0300

    rewok strategies hierarchy

--- HW_12/IThreadStrategy.cs
+++ HW_12/IThreadStrategy.cs
@@ -8,7 +8,7 @@ namespace HW_12
     internal interface IThreadStrategy<T, TResult>
     {
         public void ThreadMethod(object? obj);
-        public TResult ThreadResult(ThreadParam<T, TResult>[] threadParams);
+        public TResult? ThreadResult(ThreadParam<T, TResult>[] threadParams);
     }
 
     internal interface IInitParams
--- HW_12/ThreadParams.cs
+++ HW_12/ThreadParams.cs
@@ -1,105 +1,93 @@
 ﻿using System;
 using System.Numerics;
 using System.Text;
+using System.Threading;
 
 namespace HW_12
 {
-    internal class SumThreadStrategy<T> : IThreadStrategy<T, ulong> where T : INumber<T>
+    internal abstract class ThreadStrategyBase<T, TResult> : IThreadStrategy<T, TResult>
     {
+        protected abstract TResult? ProcessItem(T item, TResult? result, int index, ThreadParam<T, TResult> param);
+        protected abstract TResult? ProcessResultItem(TResult? item, TResult? result, ThreadParam<T, TResult> param);
+        protected abstract TResult? ProcessResult(TResult? result, params ThreadParam<T, TResult>[] threadParams);
+
         public void ThreadMethod(object? obj)
         {
-            var param = (ThreadParam<int, ulong>)obj!;
+            var param = (ThreadParam<T, TResult>)obj!;
             var span = param.Data.Span;
 
-            var sum = 0uL;
+            var result = default(TResult);
             for (int i = 0; i < span.Length; i++)
             {
                 //TODO: need recalculate indexes from the relative to absolute values, or redesign a progress handling
                 //param.ProgressUpdate(range, i);
-                sum += (ulong)span[i];
+                result = ProcessItem(span[i], result, i, param);
             }
-            param.Result = sum;
+
+            param.Result = ProcessResult(result, param);
         }
 
-        public ulong ThreadResult(ThreadParam<T, ulong>[] threadParam)
+        public TResult? ThreadResult(ThreadParam<T, TResult>[] threadParams)
         {
-            ulong result = 0;
-            foreach (var thread in threadParam)
-                result += thread.Result;
-            return result;
+            TResult? result = default;
+            foreach(var thread in threadParams)
+                result = ProcessResultItem(thread.Result, result, thread);
+            return ProcessResult(result, threadParams);
         }
     }
 
-    internal class AverageThreadStrategy<T> : IThreadStrategy<T, ulong> where T : INumber<T>
+    internal abstract class SumAverageThreadStrategyBase<T, TResult> : ThreadStrategyBase<T, TResult>
+        where T : struct, IAdditionOperators<T, TResult, TResult>, INumber<T>
+        where TResult : struct, INumber<TResult>
     {
-        public void ThreadMethod(object? obj)
-        {
-            var param = (ThreadParam<ulong, ulong>)obj!;
-            var span = param.Data.Span;
+        protected override TResult ProcessItem(T item, TResult result, int index, ThreadParam<T, TResult> param)
+            => item + result;
 
-            ulong result = 0;
-            for (int i = 0; i < span.Length; i++)
-            {
-                //TODO: need recalculate indexes from the relative to absolute values, or redesign a progress handling
-                //param.ProgressUpdate(range, i);
-                result += span[i];
-            }
-            param.Result = result / (ulong)span.Length;
-        }
-
-        public ulong ThreadResult(ThreadParam<T, ulong>[] threadParams)
-        {
-            ulong result = 0;
-            foreach (var thread in threadParams)
-                result += thread.Result;
-            return result / (ulong)threadParams.Length;
-        }
+        protected override TResult ProcessResultItem(TResult item, TResult result, ThreadParam<T, TResult> param)
+            => item + result;
     }
 
-    internal abstract class MaxMinThreadStrategy<T> : IThreadStrategy<T, T> where T : struct, INumber<T>
+    internal class SumThreadStrategy<T> : SumAverageThreadStrategyBase<T, ulong>
+        where T : struct, IAdditionOperators<T, ulong, ulong>, INumber<T>
     {
-        public void ThreadMethod(object? obj)
+        protected override ulong ProcessResult(ulong result, params ThreadParam<T, ulong>[] threadParams)
+            => result;
+    }
+
+    internal class AverageThreadStrategy<T> : SumAverageThreadStrategyBase<T, decimal>
+        where T : struct, IAdditionOperators<T, decimal, decimal>, INumber<T>
+    {
+        protected override decimal ProcessResult(decimal result, params ThreadParam<T, decimal>[] threadParams)
         {
-            var param = (ThreadParam<T, T>)obj!;
-            var span = param.Data.Span;
+            if (threadParams.Length <= 0) throw new ArgumentException(nameof(threadParams));
 
-            T result = default;
-            for (int i = 0; i < span.Length; i++)
-            {
-                if (Compare(result, span[i]) < 0)
-                    result = span[i];
-
-                //TODO: need recalculate indexes from the relative to absolute values, or redesign a progress handling
-                //param.ProgressUpdate(range, i);
-            }
-            param.Result = result;
-        }
-
-        protected abstract int Compare(T result, T compare);
-
-        public T ThreadResult(ThreadParam<T, T>[] threadParams)
-        {
-            T result = default;
-            foreach (var thread in threadParams)
-            {
-                if (Compare(result, thread.Result) < 0)
-                    result = thread.Result;
-            }
-            return result;
+            var length = threadParams.Length == 1 ? threadParams[0].Data.Length : threadParams.Length;
+            return result / length;
         }
     }
 
-    internal class MaxThreadStrategy<T> : MaxMinThreadStrategy<T> where T : struct, INumber<T>
+    internal abstract class MaxMinThreadStrategyBase<T> : ThreadStrategyBase<T, T> where T : struct, INumber<T>
     {
-        protected override int Compare(T result, T param) => result > param ? 1 : -1;
+        protected override T ProcessResult(T result, params ThreadParam<T, T>[] threadParams)
+            => result;
+
+        protected override T ProcessResultItem(T item, T result, ThreadParam<T, T> param)
+            => result;
     }
 
-    internal class MinThreadStrategy<T> : MaxMinThreadStrategy<T> where T : struct, INumber<T>
+    internal class MaxThreadStrategy<T> : MaxMinThreadStrategyBase<T> where T : struct, INumber<T>
     {
-        protected override int Compare(T result, T param) => result < param? -1 : 1;
+        protected override T ProcessItem(T item, T result, int index, ThreadParam<T, T> param)
+            => result < item ? item : result;
     }
 
-    internal class CopyThreadStrategy<T> : IInitParams, IThreadStrategy<T, T[]>
+    internal class MinThreadStrategy<T> : MaxMinThreadStrategyBase<T> where T : struct, INumber<T>
+    {
+        protected override T ProcessItem(T item, T result, int index, ThreadParam<T, T> param)
+            => result > item ? item : result;
+    }
+
+    internal class CopyThreadStrategy<T> : ThreadStrategyBase<T, T[]>, IInitParams
     {
         public Range Range { get; }
 
@@ -107,24 +95,6 @@ namespace HW_12
         {
             Range = new Range(startIndex, lastIndex);
         }
-
-        public void ThreadMethod(object? obj)
-        {
-            var param = (ThreadParam<T, T[]>)obj!;
-            var span = param.Data.Span;
-
-            T[] result = new T[span.Length];
-
-            for (int i = 0, j = 0; i < span.Length; i++, j++)
-            {
-                result[j] = span[i];
-                //TODO: need recalculate indexes from the relative to absolute values, or redesign a progress handling
-                //param.ProgressUpdate(range, i);
-            }
-            param.Result = result;
-        }
-
-        public T[] ThreadResult(ThreadParam<T, T[]>[] threadParams) => threadParams.SelectMany(s => s.Result!).ToArray();
 
         public ThreadParam<T1, TResult>[] Init<T1, TResult>(Memory<T1> data, int threadCount)
         {
@@ -139,47 +109,54 @@ namespace HW_12
 
             return result;
         }
-    }
 
-    internal abstract class FrequencyDictionaryThreadStrategy<T> : IThreadStrategy<T, Dictionary<T, int>> where T : notnull
-    {
-        public void ThreadMethod(object? obj)
+        protected override T[]? ProcessItem(T item, T[]? result, int index, ThreadParam<T, T[]> param)
         {
-            var param = (ThreadParam<T, Dictionary<T, int>>)obj!;
-            var span = param.Data.Span;
-
-            Dictionary<T, int> result = new();
-
-            for (int i = 0; i < span.Length; i++)
-            {
-                if (result.ContainsKey(span[i]))
-                    result[span[i]]++;
-                else
-                    result.Add(span[i], 1);
-
-                //TODO: need recalculate indexes from the relative to absolute values, or redesign a progress handling
-                //param.ProgressUpdate(range, i);
-            }
-            param.Result = result;
+            result![index] = item;
+            return result;
         }
 
-        public Dictionary<T, int> ThreadResult(ThreadParam<T, Dictionary<T, int>>[] threadParams)
+        protected override T[]? ProcessResult(T[]? result, params ThreadParam<T, T[]>[] threadParams)
         {
-            Dictionary<T, int> result = new();
-            foreach (var thread in threadParams)
+            return threadParams.Length == 1 ? result : threadParams.SelectMany(s => s.Result!).ToArray();
+        }
+
+        protected override T[]? ProcessResultItem(T[]? item, T[]? result, ThreadParam<T, T[]> param)
+        {
+            return item;
+        }
+    }
+
+    internal class FrequencyDictionaryThreadStrategy<T> : ThreadStrategyBase<T, Dictionary<T, int>> where T : notnull
+    {
+        protected override Dictionary<T, int>? ProcessItem(T item, Dictionary<T, int>? result, int index, ThreadParam<T, Dictionary<T, int>> param)
+        {
+            if (result!.ContainsKey(item))
+                result[item]++;
+            else
+                result.Add(item, 1);
+            return result;
+        }
+
+        protected override Dictionary<T, int>? ProcessResult(Dictionary<T, int>? result, params ThreadParam<T, Dictionary<T, int>>[] threadParams)
+        {
+            return result;
+        }
+
+        protected override Dictionary<T, int>? ProcessResultItem(Dictionary<T, int>? item, Dictionary<T, int>? result, ThreadParam<T, Dictionary<T, int>> param)
+        {
+            foreach (var value in item!)
             {
-                foreach (var param in thread.Result!)
+                if (result!.ContainsKey(value.Key))
                 {
-                    if (result.ContainsKey(param.Key))
-                    {
-                        result[param.Key] += param.Value;
-                    }
-                    else
-                    {
-                        result.Add(param.Key, param.Value);
-                    }
+                    result[value.Key] += value.Value;
+                }
+                else
+                {
+                    result.Add(value.Key, value.Value);
                 }
             }
+
             return result;
         }
     }
